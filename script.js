20e705f1b98b35e2a610a0ea00e437cfcbf0377e
Aselshine <aasel9988@gmail.com>
2022-10-01 19:39:07 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+let url = "https://www.cbr-xml-daily.ru/daily_json.js"
+let $list = document.querySelector(".list")
+let $listBtn = document.querySelector("#list")
+let $calcBtn = document.querySelector("#calc")
+let $listCurrency = document.querySelector(".listCurrency")
+let $calculatorCurrency = document.querySelector(".calculatorCurrency")
+
+
+fetch(url)
+.then(resp => resp.json())
+.then(data =>{
+    console.log(data)
+    let dataa = data.Valute
+    Object.keys(dataa).forEach(element => {
+        console.log(dataa[element])
+        let elem = dataa[element]
+        $list.insertAdjacentHTML('beforeend',`
+        <div class = "course">
+        <img src ="">
+        <h1>
+              ${elem.CharCode} <br>
+              ${elem.Nominal}
+           
+        </h1>
+             ${elem.Name}
+        <h2>
+
+        </h2>
+        
+        <h1>
+            ${elem.Value}
+            Рублей
+        </h1>
+        </div>
+        `)
+    });
+
+})
+
+$listBtn.addEventListener('click', function(){
+    $listCurrency.classList.remove('hide')
+    $calculatorCurrency.classList.add('hide')
+})
+
+$calcBtn.addEventListener('click', function(){
+    $listCurrency.classList.add('hide')
+    $calculatorCurrency.classList.remove('hide')
+})
